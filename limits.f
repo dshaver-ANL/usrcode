6fe5f2d5091f0017cee4131452361b1c043ccdf2
dshaver-ANL <dshaver@anl.gov>
2020-08-11 21:22:36 -0500

    added get_y_p subroutine

--- limits.f
+++ limits.f
@@ -136,7 +136,7 @@ C
 
       character*15 pname
       integer e,i,i0,i1,j,j0,j1,k,k0,k1,iw,jw,kw,i2,j2
-      integer ipt,wpt,estrd,isd,jsd,ifld
+      integer ipt,wpt,estrd,isd,ifld
       real msk(lx1,ly1,lz1,lelv)
       real gradu(lx1*ly1*lz1,3,3),wd(1)
       real tau(3),norm(3),vsca,tauw
@@ -488,3 +488,139 @@ c-----------------------------------------------------------------------
       return
       end
 C-----------------------------------------------------------------------
+      subroutine get_y_p(wd,phi,ifelem)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+C
+C     NOTE: min value should work if domain has internal corners
+C
+      logical ifelem
+
+      real wd(lx1,ly1,lz1,*),phi(lx1,ly1,lz1,*)
+
+      integer e,i,i0,i1,j,j0,j1,k,k0,k1,ib,jb,kb,i2,j2
+      integer ipt,isd
+c     real msk(lx1,ly1,lz1,lelv)
+      real gradu(lx1*ly1*lz1,3,3)
+      real tau(3),norm(3),vsca,tauw,dist
+      real utau,rho,mu,yp,vol
+      real glmin,glmax,glsum
+      logical ifgrad, ifdid
+
+c     data ifdid /.false./
+c     save ifdid, msk
+
+C     do some initializations once
+c     if(.not.ifdid)then
+c       ifdid=.true.
+C       build the mask  (this mask ignores some points which maybe important... 
+c       call rzero(msk,lx1*ly1*lz1*nelv)    ! need to look at it more closely)
+c       do 10 e=1,nelv                      ! the mask should be right, it's the algorithm that's wrong (see below)
+c       do 10 isd=1,2*ldim
+c         if(cbc(isd,e,1).eq.'W  ') then
+c           call backpts(i0,i1,j0,j1,k0,k1,isd)
+c           do 20 k=k0,k1
+c           do 20 j=j0,j1
+c           do 20 i=i0,i1
+c             msk(i,j,k,e)=1.0
+c 20        continue
+c         endif
+c 10    continue
+c       call dssum(msk,lx1,ly1,lz1) !for elements with edges, but not faces along a wall
+c       do 30 e=1,nelv
+c       do 30 isd=1,2*ndim
+c         if(cbc(isd,e,1).eq.'W  ') then
+c           call facind(i0,i1,j0,j1,k0,k1,lx1,ly1,lz1,isd)
+c           do 40 k=k0,k1
+c           do 40 j=j0,j1
+c           do 40 i=i0,i1
+c             msk(i,j,k,e)=0.0
+c 40        continue
+c           endif
+c 30    continue
+c     endif
+c
+c     call copy(phi,msk,lx1*ly1*lz1*nelv)
+c     return
+
+C     Now do the thing
+      call rzero(phi,lx1*ly1*lz1*nelv)
+      do e=1,nelv
+        yp=0.0
+        vol=1.0e-20
+        ifgrad=.true.
+        do isd=1,2*ndim
+          if(cbc(isd,e,1).eq.'W  ')then
+            if(ifgrad)then
+              call gradm11(gradu(1,1,1),gradu(1,1,2),gradu(1,1,3),vx,e)
+              call gradm11(gradu(1,2,1),gradu(1,2,2),gradu(1,2,3),vy,e)
+              if(if3d)
+     &         call gradm11(gradu(1,3,1),gradu(1,3,2),gradu(1,3,3),vz,e)
+              ifgrad=.false.
+            endif
+            call facind(i0,i1,j0,j1,k0,k1,lx1,ly1,lz1,isd)
+            do 50 k=k0,k1
+            do 50 j=j0,j1
+            do 50 i=i0,i1
+              kb=k
+              jb=j
+              ib=i
+              if    (isd.eq.1) then
+                jb=2
+              elseif(isd.eq.2) then
+                ib=lx1-1
+              elseif(isd.eq.3) then
+                jb=ly1-1
+              elseif(isd.eq.4) then
+                ib=2
+              elseif(isd.eq.5) then
+                kb=2
+              else
+                kb=lz1-1
+              endif
+              dist=wd(ib,jb,kb,e)
+              if(dist.gt.1.0e-12) then
+                ipt=i+(j-1)*lx1+(k-1)*lx1*ly1
+                call getSnormal(norm,i,j,k,isd,e)
+                mu=vdiff(i,j,k,e,1)
+                rho=vtrans(i,j,k,e,1)
+
+                do i2=1,ldim
+                tau(i2)=0.0
+                  do j2=1,ldim
+                    tau(i2)=tau(i2)+
+     &                   mu*(gradu(ipt,i2,j2)+gradu(ipt,j2,i2))*norm(j2)
+                  enddo
+                enddo
+
+                vsca=0.0
+                do i2=1,ldim
+                  vsca=vsca+tau(i2)*norm(i2)
+                enddo
+
+                tauw=0.0
+                do i2=1,ldim
+                  tauw=tauw+(tau(i2)-vsca*norm(i2))**2
+                enddo
+                if(tauw.gt.0.0) then
+                  tauw=sqrt(tauw)
+                  utau=sqrt(tauw/rho)
+                else
+                  tauw=0.0
+                  utau=0.0
+                endif
+                yp=yp+dist*utau*rho/mu*bm1(i,j,k,e)
+                phi(i,j,k,e)=yp
+                vol=vol+bm1(i,j,k,e)
+              endif
+  50        continue
+          endif
+        enddo
+        if(ifelem) call cfill(phi(1,1,1,e),yp/vol,lx1*ly1*lz1)
+      enddo
+
+      return
+      end
+C-----------------------------------------------------------------------
+
